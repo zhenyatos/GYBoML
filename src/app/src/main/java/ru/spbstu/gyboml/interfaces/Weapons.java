572c728d051f68be95d3576b56fb38b74230cfee
= <=>
2020-03-02 00:30:38 +0300

    Weapons interface create

---
+++ src/app/src/main/java/ru/spbstu/gyboml/interfaces/Weapons.java
@@ -0,0 +1,9 @@
+package ru.spbstu.gyboml.interfaces;
+
+public interface Weapons {
+    void setMaxStartVelocity(float velocity);
+    float getMaxStartVelocity();
+    void setMass(float mass);
+    float getMass();
+    
+}
