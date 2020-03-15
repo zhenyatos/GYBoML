fb97530a45366e93f0798f8d6e0fa04c0556b86d
kystyn <konstantindamaskinskit@gmail.com>
2020-03-16 00:50:03 +0300

    Rename root 'src' directory to 'client'

---
+++ client/app/src/test/java/ru/spbstu/gyboml/EventSystemTest.java
@@ -0,0 +1,20 @@
+package ru.spbstu.gyboml;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import ru.spbstu.gyboml.utils.EventSystem;
+
+public class EventSystemTest {
+    public EventSystemTest() {
+
+    }
+    
+    @Test
+    public void testConnect() {
+
+    }
+
+    EventSystem eventSystem;
+}
+
