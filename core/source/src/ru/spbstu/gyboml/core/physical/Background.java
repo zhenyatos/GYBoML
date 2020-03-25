9992afec80a0bd72d6fc6fe11804c1c1d03647f8
Jiija <avarisov@yandex.ru>
2020-03-25 21:05:03 +0300

    Add TestClient, MainTest and Background classes

---
+++ core/source/src/ru/spbstu/gyboml/core/physical/Background.java
@@ -0,0 +1,21 @@
+package ru.spbstu.gyboml.core.physical;
+
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.World;
+import com.codeandweb.physicseditor.PhysicsShapeCache;
+
+import java.io.File;
+
+public class Background {
+    private final Body body;
+    public Background(World world, float x, float y, float SCALE) {
+        File file = new File("./res/physics/background.xml");
+        PhysicsShapeCache physicsBodies = new PhysicsShapeCache(new FileHandle(file));
+        body = physicsBodies.createBody("bg_land", world, SCALE, SCALE);
+        body.setTransform(x, y, 0);
+        body.setType(BodyDef.BodyType.StaticBody);
+    }
+
+}
