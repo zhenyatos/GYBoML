8382562074cfa9ea49f2c32b83c293dc7d4a58f1
zhenyatos <zhenyatos999@gmail.com>
2020-03-28 02:14:37 +0300

    Add some cannon-tower connection experiments

--- core/source/src/ru/spbstu/gyboml/core/physical/PhysicalTower.java
+++ core/source/src/ru/spbstu/gyboml/core/physical/PhysicalTower.java
@@ -1,9 +1,15 @@
 package ru.spbstu.gyboml.core.physical;
 
 import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Joint;
+import com.badlogic.gdx.physics.box2d.JointDef;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.physics.box2d.joints.RevoluteJoint;
+import com.badlogic.gdx.physics.box2d.joints.RevoluteJointDef;
 import com.codeandweb.physicseditor.PhysicsShapeCache;
 
 import java.io.File;
@@ -26,12 +32,15 @@ public class PhysicalTower extends Tower {
         tower = physicsShapeCache.createBody("tower" + playerName + "base", world, pos.scale, pos.scale);
         cannon = physicsShapeCache.createBody("tower" + playerName + "cannon", world, pos.scale, pos.scale);
 
-        tower.setTransform(pos.x, pos.y, 0);
-        cannon.setTransform(pos.x, pos.y, 0);
-
         // Tower doesn't move => static
         tower.setType(BodyDef.BodyType.StaticBody);
         // Cannon can move, but doesn't interact => kinematic
-        cannon.setType(BodyDef.BodyType.KinematicBody);
+        cannon.setType(BodyDef.BodyType.DynamicBody);
+        cannon.setGravityScale(0.f);
+
+        tower.setTransform(pos.x, pos.y, 0);
+        cannon.setTransform(pos.x + (40.f * pos.scale), pos.y + (760.f * pos.scale), 0);
+        tower.setActive(false);
+        cannon.setAngularVelocity(1.f);
     }
 }
